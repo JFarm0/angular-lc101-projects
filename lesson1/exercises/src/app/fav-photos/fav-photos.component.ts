90220f9ad3d56b8f7120a12cce7d32479389e858
J Farmer <JFarm0>
2021-03-28 21:07:08 -0500

    Part 1 done

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Take a look at these here photographs!';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://www.6-string.com/media/catalog/product/e/s/esp_ltd_f-4e_ns_ebony_sampleprototype_bass_guitar_5_.jpg';
+  image3 = 'https://megamusic.blob.core.windows.net/images/0018834_ibanez-rga60al-electric-guitar-antique-brown-stained-low-gloss.jpg';
 
   constructor() { }
 
