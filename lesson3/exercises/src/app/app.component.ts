1ea017a528e36b6f41efd8bf6d9c78bffbcf9e48
J Farmer <JFarm0>
2021-04-04 21:42:36 -0500

    Angular Part 3 Nearly Done

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,69 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled: boolean = true;
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+      this.color = 'blue';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+      this.takeOffEnabled = false;
+    }
+  }
+
+  handleLanding() {
+    window.alert('The shuttle is landing. Landing gear engaged.');
+    this.color = 'green';
+    this.height = 0;
+    this.message = 'The shuttle has landed.';
+    this.takeOffEnabled = true;
+  }
+
+  handleAbort() {
+    let result = window.confirm('Do you really want to abort?');
+    if (result) {
+      this.message = 'Mission aborted.';
+      this.color = 'red';
+      this.height = 0;
+      this.takeOffEnabled = true;
+    }
+  }
+
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    }
+
+    if (direction === 'left') {
+      let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    }
+
+    if (direction === 'up') {
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height += 10000;
+    }
+
+    if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height -= 10000;
+    }
+  }
+
+  // edgeWarning(rocketImage) {
+  //   let xPosition = rocketImage.style.left;
+  //   let yPosition = rocketImage.style.bottom;
+  //   if (xPosition < 0 || yPosition < 0) {
+  //     this.color = 'orange';
+  //   }
+  // }
+
 }
